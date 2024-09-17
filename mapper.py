f1f4f16fa36554e985dec4bfd2d220b8cacf490f
max vorabhol <max003003003@gmail.com>
2024-09-17 14:54:51 +0700

    reformat code

--- mapper.py
+++ mapper.py
@@ -4,5 +4,5 @@ from operator import itemgetter
 import sys
 
 for line in sys.stdin:
-	words = line.split(",")
-	print(words[0]+"\t"+words[1]+"\t"+words[2][:-1]+"\t1")
+    words = line.split(",")
+    print(words[0] + "\t" + words[1] + "\t" + words[2][:-1] + "\t1")
